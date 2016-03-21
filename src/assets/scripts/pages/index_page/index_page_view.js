e44c996bdd8ddfad552cf74a5f1482abe2c10a9c
BlueHotDog <dannifriedland@gmail.com>
2016-03-21 01:53:31 -0700

    fixing maurann audio

--- src/assets/scripts/pages/index_page/index_page_view.js
+++ src/assets/scripts/pages/index_page/index_page_view.js
@@ -6,7 +6,14 @@ const IndexPageView = BaseView.extend(
     {
       el: 'main.index-page',
       onRender: function(){
-        
+        var maurann = $('.maurann-name-audio')[0];
+        $('.maurann-name').mouseenter(function() {
+          maurann.play();
+          $(this).addClass('active');
+          setTimeout(function() {
+            $('.maurann-name').removeClass('active');
+          }, 350);
+        });
       }
 
     }
